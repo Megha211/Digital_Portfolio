e0ba766fd18680aef577ac1819db86d888fce7c9
Megha211 <127596576+Megha211@users.noreply.github.com>
2024-06-28 22:48:15 +0530

    desc change

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -46,9 +46,9 @@ function Projects() {
               imgPath={met}
               isBlog={false}
               title="Video Conferencing Web App "
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              description='Met is a WebRTC-powered video conferencing app with secure authentication, Google Sign-In, and CAPTCHA verification. It offers high-quality audio/video calls, real-time messaging, group chats, screen sharing, and MESH topology for live discussions.'
               ghLink="https://github.com/Megha211/Met.git"
-              demoLink="met-snowy.vercel.app"              
+              demoLink="https://met-snowy.vercel.app/"              
             />
           </Col>
 
@@ -58,8 +58,7 @@ function Projects() {
               isBlog={false}
               title="Localizer"
               description="Led the creation of an innovative system for tracking students both indoors and outdoors using RFID and GPS technologies, significantly improving student safety and monitoring efficiency."
-              // ghLink="https://github.com/soumyajit4419/Plant_AI"
-              // demoLink="https://plant49-ai.herokuapp.com/"
+              demoLink="https://localizer.cyclic.app/"
             />
           </Col>
         </Row>
